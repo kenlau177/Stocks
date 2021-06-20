9f16a05f2914e87ed66188730b38f7ffe71ac9ff
ken <klau@hotheadgames.com>
2021-06-20 00:37:11 -0700

    Added new files

---
+++ incremental_run.R
@@ -0,0 +1,26 @@
+setwd("C://Users//klau//Desktop//Work//Github_Ken//Stocks")
+
+rm(list=ls())
+
+library(dplyr)
+library(lubridate)
+library(reshape2)
+library(glue)
+library(ggplot2)
+library(scales)
+library(tidyquant)
+library(rvest)
+library(magrittr)
+library(stringr)
+
+# 1 -- Update data
+# 2 -- Transform and Output Report
+
+# symbol, date, close
+
+raw_current = read.csv("")
+
+
+
+
+
