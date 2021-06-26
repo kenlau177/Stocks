6876ace548e382d328dac8056424d8e26184e2c0
ken <klau@hotheadgames.com>
2021-06-26 00:50:55 -0700

    updated files

--- incremental_run.R
+++ incremental_run.R
@@ -18,9 +18,35 @@ library(stringr)
 
 # symbol, date, close
 
-raw_current = read.csv("")
+# One-time modify file
+# raw = read.csv("prices_df.csv")
+# raw = raw[,c("symbol","date","close")]
+# write.csv(raw, "prices.csv", row.names=F)
+#
 
+raw_current = read.csv("prices.csv", stringsAsFactors=F)
 
+dat_current = raw_current
+
+symbols = unique(dat_current$symbol)
+
+from1 = max(dat_current$date)
+to1 = Sys.Date() - 1
+
+if(to1 > from1) {
+  dat_new = tq_get(symbols, from=from1, to=to1, get="stock.prices")
+  dat_new = dat_new[,c("symbol","date","close")]
+  dat_new$date = as.character(dat_new$date)
+  dat_current = rbind(dat_current, dat_new)
+}
+
+range(dat_current$date)
+
+min_date_keep = as.character(max(to1) - 8*365)
+
+out = filter(dat_current, date>=min_date_keep)
+
+write.csv(out, "prices.csv", row.names=F)
 
 
 
